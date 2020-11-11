6dae7b7a69a907ee20c0dc1911865e0434671fec
modlanglais <morgandlanglais@gmail.com>
2020-11-11 11:42:27 -0600

    Moves creds to env file

--- cos_backup.py
+++ cos_backup.py
@@ -1,11 +1,14 @@
 import ibm_boto3
 from ibm_botocore.client import Config, ClientError
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-source_apikey = "{api-key}"
-source_resource_crn = "{resource-crn}"
-source_auth_endpoint = "{auth-endpoint}"
-source_endpoint = "{endpoint}"
-source_bucket_name = "{bucket-name}"
+source_apikey = os.getenv("SOURCEAPIKEY")
+source_resource_crn = os.getenv("RESOURCECRN")
+source_auth_endpoint = os.getenv("AUTHENDPOINT")
+source_endpoint = os.getenv("ENDPOINT")
+source_bucket_name = os.getenv("BUCKETNAME")
 
 cos = ibm_boto3.resource("s3",
     ibm_api_key_id=source_apikey,
